772b4ea10b8d4d3e5a604c34366b076113679842
krounare <krounarezki308@gmail.com>
2022-04-08 13:13:37 +0200

    third commit

--- src/main/java/fr/univrouen/rss22/Controllers/IndexController.java
+++ src/main/java/fr/univrouen/rss22/Controllers/IndexController.java
@@ -1,7 +1,15 @@
 package fr.univrouen.rss22.Controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import model.Item;
+
+
+
 
 	@RestController
 	public class IndexController {
@@ -9,6 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 	public String index() {
 	return "Hello rss22 !";
 	}
+
+	
+
 	}
+	
 
 
